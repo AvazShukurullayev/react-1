92f3b8bfb93471f76e0034922129656a159ff518
Avaz <elonarm21@gmail.com>
2024-01-09 01:04:18 +0500

    added bootstrap last version

---
+++ react-1/src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (<div className={"app"}>
+        <div className="container">
+            <h1 className={"text-center"}> Hello world</h1>
+        </div>
+    </div>)
+};
+
+export default App;
